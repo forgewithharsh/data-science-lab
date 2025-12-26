fcbe401cd0db0eea44be17735e3410d7acbe28d0
Harsh Guleria <guleriah59@gmail.com>
2025-12-26 23:45:48 +0530

    static and templates folder

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,6 +8,10 @@ def hello_world():
 
 @app.route("/about")
 def about():
-    return "<p>Hello, I am in about page!</p>"
+    return render_template("index.html")
+
+@app.route("/contact")
+def contact():
+    return "<p>Hello, I am in contact page!</p>"
 
 app.run(debug=True)
