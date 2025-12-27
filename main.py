db8aa5def4e5f7789caf0466d51144ff6dbf0403
Harsh Guleria <guleriah59@gmail.com>
2025-12-27 13:21:20 +0530

    handling forms in flask

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__,static_folder = "assests", static_url_path="/files")
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def hello_world():
+    if request.method == "POST":
+        name = request.form["email"]
+        password = request.form["password"]
+        print(f"The name is {name} and the password is {password}")
+        return "<b>Thanks for using facebook. You are logged in</b>"
+    # save it to the database
     return render_template("index.html")
 
 @app.route("/about")
