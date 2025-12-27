efd10bd88e7a36025f1579362d2d64c4f1c14132
Harsh Guleria <guleriah59@gmail.com>
2025-12-27 11:15:15 +0530

    changing static path and static folder

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder = "assests", static_url_path="/files")
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
-    return render_template("index.html")
+    return render_template("about.html")
 
 @app.route("/contact")
 def contact():
