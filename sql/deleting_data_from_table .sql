51f7f47b021b9f17d2e5f7a3f23a9047bdb631bd
Harsh Guleria <guleriah59@gmail.com>
2026-01-02 17:11:29 +0530

    Deleting data from a Table

---
+++ sql/deleting_data_from_table .sql
@@ -0,0 +1,11 @@
+-- SELECT * FROM student;
+
+-- DELETE FROM student WHERE id = 3;
+
+-- DELETE FROM student WHERE age < 20;
+
+-- DELETE FROM student;
+
+DROP TABLE student;
+
+-- SELECT * FROM student;
