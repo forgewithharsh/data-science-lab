d198bfcf81435840b07c3970052dde4a35494b77
Harsh Guleria <guleriah59@gmail.com>
2026-01-08 15:31:06 +0530

    Visualizing Tons of Data

--- matplotlib/matplotlib_demo.py
+++ matplotlib/matplotlib_demo.py
@@ -12,8 +12,8 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-for i in range(50):
-    plt.plot(np.random.rand(100), linewidth=1)
+
+plt.plot(np.random.rand(100), linewidth=1)
 
 plt.title("Too Much Data Can Be Confusing!")
 plt.grid(True)
