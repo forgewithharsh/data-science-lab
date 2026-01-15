3c0a1ad2b6e125344fd5950afe0565ed94c1ab29
Harsh Guleria <guleriah59@gmail.com>
2026-01-15 13:45:09 +0530

    Fix embedding prompt in create_embedding function and update JSON chunk processing

--- rag-based-ai/real_chunks.py
+++ rag-based-ai/real_chunks.py
@@ -1,13 +1,25 @@
 import requests
+import os
+import json
 
 def create_embedding(text):
     r = requests.post("http://localhost:11434/api/embeddings", json={
         "model": "bge-m3",
-        "prompt": "text"
+        "prompt": text
     })
 
     embedding = r.json()['embedding']
     return embedding
 
-a = create_embedding("Cat sat on the mat")
-print(a)
+jsons = [f for f in os.listdir("jsons") if f.endswith(".json")]
+
+for json_file in jsons:
+    with open(f"jsons/{json_file}", "r", encoding="utf-8", errors="ignore") as f:
+        content = json.loads(f.read())
+
+    for chunk in content["chunks"]:
+        print(chunk)
+    break
+
+# a = create_embedding("Cat sat on the mat")
+# print(a)
